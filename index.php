513157c85d1bf1face38ce53c395c196b4e7f33d
Quoc-Vinh <80906588+Quoc-Vinh@users.noreply.github.com>
2021-05-18 22:36:39 +0700

    Add files via upload
    index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+	<meta charset="utf8">
+ </head>
+ <body>
+	</form>
+	
+ 	 <br><a href="create_db.php"> Create database </a>
+	<br><a href="add_student.php"> Add student </a>
+	<br><a href="list_students.php">List student</a>
+		
+ </body>
+</html>
