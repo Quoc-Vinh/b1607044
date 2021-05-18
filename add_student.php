2759e0bee93f79d7a3c84790cd247e1600df4035
Quoc-Vinh <80906588+Quoc-Vinh@users.noreply.github.com>
2021-05-19 00:10:37 +0700

    Add files via upload

---
+++ add_student.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<style>
+			.error {color: #FF0000;}
+		</style>
+	</head>
+	<body>
+		<h2>PHP Form Valication Example</h2>
+		<form method="post">
+			 <p>MSSV</p><input type="text" name="mssv" value=""><br>
+			 <p>Họ Tên</p><input type="text" name="hoten" value=""><br>
+			 <input type="submit" name="submit" value="Submit">
+		</form>
+		<?php
+			$username=$_POST["mssv"];
+			$password=$_POST["hoten"];
+			function pg_connection_string_from_database_url(){
+				extract(parse_url($_ENV["DATABASE_URL"]));
+				return "user=$user password=$pass host=$host dbname=".substr($path,1);
+			}
+			$db = pg_connect(pg_connection_string_from_database_url());
+			if(!$db){
+				echo "Eror : Unable to open database\n";
+			}else{
+				echo "Opened database successfully\n";
+			}
+			$sql="INSERT INTO student (mssv, hoten) VALUES ('$mssv','$hoten')";
+			$ret = pg_query($db,$sql);
+			if(!$ret){
+				echo pg_last_error($db);
+			}else{
+				echo "Insert successfully\n";
+				echo "$sql";
+			
+			}
+			pg_close($db);?>
+	</body>
+</html>
---
+++ create_db.php
@@ -0,0 +1,20 @@
+<?php
+function pg_connection_string_from_database_url(){
+				extract(parse_url($_ENV["postgres://jitiarrkuzxgkz:33640b35808455ae6fba89cfee97cffb217b735b3d9c14d624cf3e7d4665d298@ec2-18-215-111-67.compute-1.amazonaws.com:5432/d567ufs33npl7d"]));
+				return "$user="jitiarrkuzxgkz" $password="33640b35808455ae6fba89cfee97cffb217b735b3d9c14d624cf3e7d4665d298" $host="ec2-18-215-111-67.compute-1.amazonaws.com" $dbname="d567ufs33npl7d".substr($path,1);
+			}
+			$db = pg_connect(pg_connection_string_from_database_url());
+			if(!$db){
+				echo "Eror : Unable to open database\n";
+			}else{
+				echo "Opened database successfully\n";
+			}
+  $sql = "CREATE TABLE student (mssv CHAR(10) PRIMARY KEY NOT NULL, hoten CHAR(50))";
+  $ret = pg_query($db,$sql);
+  if(!$ret){
+     echo pg_last_error($db);
+  }else {
+      echo "Table create student successfully\n";
+  }
+  pg_close($db);
+?>
