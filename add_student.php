c46cf69e1afc495ac1022c822b1f1a5b6147de2d
Quoc-Vinh <80906588+Quoc-Vinh@users.noreply.github.com>
2021-05-19 11:28:27 +0700

    Update add_student.php

--- add_student.php
+++ add_student.php
@@ -13,8 +13,8 @@
 			 <input type="submit" name="submit" value="Submit">
 		</form>
 		<?php
-			$username=$_POST["mssv"];
-			$password=$_POST["hoten"];
+			$mssv=$_POST["mssv"];
+			$hoten=$_POST["hoten"];
 			function pg_connection_string_from_database_url(){
 				extract(parse_url($_ENV["DATABASE_URL"]));
 				return "user=$user password=$pass host=$host dbname=".substr($path,1);
